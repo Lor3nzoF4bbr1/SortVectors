944a2af35a86109133c3d3a218d85da952088e07
Lorenzo Fabbri <LORENZO.FABBRI.4G@local.ittcesena.it>
2020-10-07 11:06:53 +0200

    First step

---
+++ SortVectors/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace SortVectors
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the cumzone");
+
+            int[] vect = new int[] { 4, -1, 100, -85, 4, 6, 77, -35, 2, 0 };
+
+            foreach (int v in vect) 
+            Console.WriteLine(v);
+            
+            Console.ReadKey();
+        }
+    }
+}
